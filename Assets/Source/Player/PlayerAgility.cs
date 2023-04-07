53df658ef84e2ccf30bbd261a0115913414aca3f
Aleksey Saskevich <saskevich.aleksey@gmail.com>
2023-04-07 15:46:18 +0300

    Improve PlayerAgility

--- Assets/Source/Player/PlayerAgility.cs
+++ Assets/Source/Player/PlayerAgility.cs
@@ -1,33 +1,56 @@
-﻿using UnityEngine;
+﻿using Source.Combo;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace Source.Player
 {
     public sealed class PlayerAgility : MonoBehaviour
     {
-        [SerializeField] private float _maxAgility;
+        [SerializeField] private PlayerCombo _playerCombo;
         [SerializeField] private float _increasingAgility;
-        
+        [SerializeField] private float _decreasingAgility;
+
+        public event UnityAction AgilityChanged;
+
+        [field: SerializeField] public float MaxAgility { get; private set; }
         public float Agility { get; private set; }
 
-        private void Start()
+        private void Awake()
         {
-            Agility = _maxAgility;
+            Agility = MaxAgility;
+        }
+
+        private void OnEnable()
+        {
+            _playerCombo.Attacked += OnAttacked;
+        }
+
+        private void OnDisable()
+        {
+            _playerCombo.Attacked -= OnAttacked;
         }
 
         private void Update()
         {
-            if (Agility >= _maxAgility)
-                return;
-            
-            Agility += _increasingAgility;
+            if (_playerCombo.CurrentState is IdleState && Agility < MaxAgility)
+                IncreaseAgility();
         }
 
-        public void TryTakeAgility(float value)
+        private void OnAttacked()
         {
-            if (value <= 0)
-                return;
+            DecreaseAgility();
+        }
 
-            Agility -= value;
+        private void IncreaseAgility()
+        {
+            Agility = Mathf.Clamp(Agility + _increasingAgility, 0, MaxAgility);
+            AgilityChanged?.Invoke();
+        }
+
+        private void DecreaseAgility()
+        {
+            Agility = Mathf.Clamp(Agility - _decreasingAgility, 0, MaxAgility);
+            AgilityChanged?.Invoke();
         }
     }
 }
