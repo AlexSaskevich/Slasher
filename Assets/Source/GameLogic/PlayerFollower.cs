31ca413b66c909726eb8d267bb42bb9ca2ad5411
Aleksey Saskevich <saskevich.aleksey@gmail.com>
2023-04-12 08:39:44 +0300

    Improve PlayerFollower

--- Assets/Source/GameLogic/PlayerFollower.cs
+++ Assets/Source/GameLogic/PlayerFollower.cs
@@ -6,32 +6,22 @@ namespace Source.GameLogic
     public sealed class PlayerFollower : MonoBehaviour, IMoveable
     {
         [SerializeField] private Transform _player;
+        [SerializeField] private Vector3 _offset;
 
-        private Vector3 _offence;
-        private Vector3 _position;
-        private Vector3 _playerPosition;
-
-        public float DefaultSpeed => 1;
-
-        private void Start()
-        {
-            _playerPosition = _player.position;
-            _position = transform.position;
-
-            _offence = new Vector3(_position.x - _playerPosition.x, 0, _position.z - _playerPosition.z);
-        }
+        [field: SerializeField] public float DefaultSpeed { get; private set; }
 
         private void LateUpdate()
         {
-            _playerPosition = _player.position;
-            _position = transform.position;
-
-            Move(_playerPosition.x + _offence.x, _playerPosition.z + _offence.z);
+            Vector3 desiredPosition = _player.position + _offset;
+            Move(desiredPosition.x, desiredPosition.z);
         }
 
         public void Move(float directionX, float directionZ)
         {
-            transform.position = new Vector3(directionX, transform.position.y, directionZ);
+            Vector3 targetPosition = new(directionX, _offset.y, directionZ);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, DefaultSpeed * Time.deltaTime);
+
+            transform.position = smoothedPosition;
         }
     }
 }
