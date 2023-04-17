e79ed286f3daf117500373cda7b64b86d8956a91
daniilkalmykov <daniilk24960@gmail.com>
2023-04-17 23:36:27 +0600

    add player wallet with view

---
+++ Assets/Source/Player/PlayerWallet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Source.Player
+{
+    public sealed class PlayerWallet
+    {
+        private int _currentMoney;
+
+        public event Action<int> MoneyChanged;
+
+        public void TryTakeMoney(int money)
+        {
+            if(money <= 0)
+                return;
+
+            _currentMoney += money;
+
+            MoneyChanged?.Invoke(_currentMoney);
+        }
+    }
+}
---
+++ Assets/Source/UI/PlayerWalletView.cs
@@ -0,0 +1,34 @@
+﻿using Source.Player;
+using TMPro;
+using UnityEngine;
+
+namespace Source.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public sealed class PlayerWalletView : MonoBehaviour
+    {
+        private TMP_Text _money;
+        
+        private readonly PlayerWallet _playerWallet = new();
+
+        private void Awake()
+        {
+            _money = GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            _playerWallet.MoneyChanged += OnMoneyChanged;
+        }
+
+        private void OnDisable()
+        {
+            _playerWallet.MoneyChanged -= OnMoneyChanged;
+        }
+
+        private void OnMoneyChanged(int money)
+        {
+            _money.text = money.ToString();
+        }
+    }
+}
