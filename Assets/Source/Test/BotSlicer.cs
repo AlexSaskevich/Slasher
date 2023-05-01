db2f98c7b8998a7aff1a922b4ca077fa7d0db0ad
AlexSaskevich <saskevich.aleksey@gmail.com>
2023-05-01 11:56:22 +0300

    Improve BotSlicer

--- Assets/Source/Test/BotSlicer.cs
+++ Assets/Source/Test/BotSlicer.cs
@@ -1,4 +1,4 @@
-﻿using Source.Bot;
+﻿using Source.GameLogic;
 using UnityEngine;
 
 namespace Source.Test
@@ -7,26 +7,27 @@ namespace Source.Test
     {
         [SerializeField] private Rigidbody[] _parts;
         [SerializeField] private Collider[] _colliders;
-        [SerializeField] private BotHumanHealth _botHumanHealth;
-        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
-        [SerializeField] private CapsuleCollider _capsuleCollider;
         [SerializeField] private float _force;
 
+        private SkinnedMeshRenderer _skinnedMeshRenderer;
+        private Health _botHealth;
         private Animator _animator;
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
+            _botHealth = GetComponent<Health>();
+            _skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
         }
 
         private void OnEnable()
         {
-            _botHumanHealth.Died += OnDied;
+            _botHealth.Died += OnDied;
         }
 
         private void OnDisable()
         {
-            _botHumanHealth.Died -= OnDied;
+            _botHealth.Died -= OnDied;
         }
 
         private void Start()
@@ -37,22 +38,32 @@ namespace Source.Test
 
         private void OnDied()
         {
+            if (_botHealth.enabled == false)
+                return;
+
+            DisableBot();
+
+            EnableBotSlices();
+        }
+
+        private void DisableBot()
+        {
+            _botHealth.enabled = false;
             _skinnedMeshRenderer.enabled = false;
             _animator.enabled = false;
 
-            DisableColliders();
+            for (int i = 0; i < _colliders.Length; i++)
+                _colliders[i].enabled = false;
+        }
 
+        private void EnableBotSlices()
+        {
             for (int i = 0; i < _parts.Length; i++)
             {
                 _parts[i].gameObject.SetActive(true);
+                _parts[i].transform.parent = transform;
                 _parts[i].AddForce(Vector3.up * _force);
             }
-        }
-
-        private void DisableColliders()
-        {
-            for (int i = 0; i < _colliders.Length; i++)
-                _colliders[i].enabled = false;
         }
     }
 }
