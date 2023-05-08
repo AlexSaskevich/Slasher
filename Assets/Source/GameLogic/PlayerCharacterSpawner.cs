ce526d939330a50958824b1b012f0df7f7a7a2e3
daniilkalmykov <daniilk24960@gmail.com>
2023-05-08 20:33:09 +0600

    remove ObjectsInitializer

--- Assets/Source/GameLogic/PlayerCharacterSpawner.cs
+++ Assets/Source/GameLogic/PlayerCharacterSpawner.cs
@@ -5,6 +5,13 @@ using Source.UI.Buttons.ControlButtons;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Source.GameLogic.Boosters;
+using Source.GameLogic.BotsSpawners;
+using Source.Skills;
+using Source.UI.Bars;
+using Source.UI.Buttons.UIButtons;
+using Source.UI.Views;
+using Source.UI.Views.SkillViews;
 using Source.Yandex;
 using UnityEngine;
 
@@ -16,19 +23,44 @@ namespace Source.GameLogic
         [SerializeField] private Joystick _joystick;
         [SerializeField] private List<ControlButton> _controlButtons;
         [SerializeField] private AdShower _adShower;
+        [SerializeField] private PlayerFollower _playerFollower;
+        [SerializeField] private PlayerAgilityBar _playerAgilityBar;
+        [SerializeField] private PlayerHealthBar _playerHealthBar;
+        [SerializeField] private PlayerManaBar _playerManaBar;
+        [SerializeField] private PlayerWalletView _playerWalletView;
+        [SerializeField] private BuffCooldownView _buffCooldownView;
+        [SerializeField] private BuffEffectView _buffEffectView;
+        [SerializeField] private UltimateCooldownView _ultimateCooldownView;
+        [SerializeField] private UltimateEffectView _ultimateEffectView;
+        [SerializeField] private RollCooldownView _rollCooldownView;
+        [SerializeField] private RollEffectView _rollEffectView;
+        [SerializeField] private List<BotsSpawner> _botsSpawners;
+        [SerializeField] private BuyCharacterButton _buyCharacterButton;
+        [SerializeField] private List<BoostBlinder> _boostBlinders;
+        [SerializeField] private MoneyButton _moneyButton;
 
         private readonly List<PlayerCharacter> _playerCharacters = new();
 
         private PlayerCharacter _currentCharacter;
 
-        public event Action<PlayerCharacter> PlayerSet;
-
-        private void Start()
+        private void Awake()
         {
             GameProgressSaver.SetCharacterBoughtStatus(PlayerCharacterName.Biker, true);
 
             InitPlayerCharacters();
             SetCurrentPlayer();
+        }
+
+        private void OnEnable()
+        {
+            if (_buyCharacterButton != null)
+                _buyCharacterButton.CharacterSet += OnCharacterSet;
+        }
+
+        private void OnDisable()
+        {
+            if (_buyCharacterButton != null)
+                _buyCharacterButton.CharacterSet -= OnCharacterSet;
         }
 
         public IEnumerable<PlayerCharacter> GetPlayerCharacters()
@@ -44,7 +76,7 @@ namespace Source.GameLogic
             return _playerCharacters[index];
         }
 
-        public void SetCurrentPlayer()
+        private void SetCurrentPlayer()
         {
             if (_currentCharacter != null)
                 _currentCharacter.gameObject.SetActive(false);
@@ -59,11 +91,15 @@ namespace Source.GameLogic
                 throw new ArgumentNullException();
 
             _currentCharacter = playerCharacter;
-            
-            PlayerSet?.Invoke(playerCharacter);
 
+            InitObjects(playerCharacter);
             playerCharacter.gameObject.SetActive(true);
             GameProgressSaver.SetCurrentCharacterIndex((int)playerCharacter.PlayerCharacterName);
+        }
+
+        private void OnCharacterSet()
+        {
+            SetCurrentPlayer();
         }
 
         private void InitPlayerCharacters()
@@ -89,9 +125,145 @@ namespace Source.GameLogic
 
                 if (_adShower != null)
                     playerHealth.Init(_adShower);
-                
+
                 _playerCharacters.Add(character);
                 character.gameObject.SetActive(false);
+            }
+
+        }
+
+        private void InitObjects(PlayerCharacter playerCharacter)
+        {
+            if (_playerFollower != null)
+                _playerFollower.Init(playerCharacter.transform);
+
+            if (_playerAgilityBar != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerAgility playerAgility) == false)
+                    throw new ArgumentNullException();
+
+                _playerAgilityBar.Init(playerAgility);
+            }
+
+            if (_playerHealthBar != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerHealth playerHealth) == false)
+                    throw new ArgumentNullException();
+
+                _playerHealthBar.Init(playerHealth);
+            }
+
+            if (_playerManaBar != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerMana playerMana) == false)
+                    throw new ArgumentNullException();
+
+                _playerManaBar.Init(playerMana);
+            }
+
+            if (_playerWalletView != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerWallet playerWallet) == false)
+                    throw new ArgumentNullException();
+
+                _playerWalletView.Init(playerWallet);
+            }
+
+            if (_buffCooldownView != null && _buffEffectView != null)
+            {
+                if (playerCharacter.TryGetComponent(out Buff buff) == false)
+                    throw new ArgumentNullException();
+
+                _buffCooldownView.Init(buff);
+                _buffEffectView.Init(buff);
+            }
+
+            if (_ultimateCooldownView != null && _ultimateEffectView != null)
+            {
+                if (playerCharacter.TryGetComponent(out Ultimate ultimate) == false)
+                    throw new ArgumentNullException();
+
+                _ultimateCooldownView.Init(ultimate);
+                _ultimateEffectView.Init(ultimate);
+            }
+
+            if (_rollCooldownView != null && _rollEffectView != null)
+            {
+                if (playerCharacter.TryGetComponent(out Roll roll) == false)
+                    throw new ArgumentNullException();
+
+                _rollCooldownView.Init(roll);
+                _rollEffectView.Init(roll);
+            }
+
+            if (_buyCharacterButton != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerWallet playerWallet) == false)
+                    throw new ArgumentNullException();
+
+                _buyCharacterButton.Init(playerWallet);
+            }
+
+            if (_moneyButton != null)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerWallet playerWallet) == false)
+                    throw new ArgumentNullException();
+
+                _moneyButton.Init(playerWallet);
+            }
+
+            foreach (var boostBlinder in _boostBlinders)
+            {
+                if (playerCharacter.TryGetComponent(out PlayerWallet playerWallet) == false)
+                    throw new ArgumentNullException();
+
+                switch (boostBlinder.GoodStatus)
+                {
+                    case GoodStatus.HealthUpgradeable:
+                    {
+                        if (playerCharacter.TryGetComponent(out PlayerHealth playerHealth) == false)
+                            throw new ArgumentNullException();
+
+                        boostBlinder.Init(playerWallet, playerHealth, playerCharacter.PlayerCharacterName);
+                        break;
+                    }
+                    case GoodStatus.ManaUpgradeable:
+                    {
+                        if (playerCharacter.TryGetComponent(out PlayerMana playerMana) == false)
+                            throw new ArgumentNullException();
+
+                        boostBlinder.Init(playerWallet, playerMana, playerCharacter.PlayerCharacterName);
+                        break;
+                    }
+                    case GoodStatus.AgilityUpgradeable:
+                    {
+                        if (playerCharacter.TryGetComponent(out PlayerAgility playerAgility) == false)
+                            throw new ArgumentNullException();
+
+                        boostBlinder.Init(playerWallet, playerAgility, playerCharacter.PlayerCharacterName);
+                        break;
+                    }
+                    default:
+                        throw new ArgumentNullException();
+                }
+            }
+
+            if (_botsSpawners.Count == 0)
+                return;
+
+            foreach (var botsSpawner in _botsSpawners.Where(botsSpawner => botsSpawner != null))
+            {
+                if (playerCharacter.TryGetComponent(out PlayerMovement playerMovement) == false)
+                    throw new ArgumentNullException();
+
+                if (playerCharacter.TryGetComponent(out PlayerWallet playerWallet) == false)
+                    throw new ArgumentNullException();
+
+                if (playerCharacter.TryGetComponent(out PlayerHealth playerHealth) == false)
+                    throw new ArgumentNullException();
+
+                botsSpawner.Init(playerMovement, playerHealth, playerWallet,
+                    playerCharacter.TryGetComponent(out ZombieScore zombieScore) ? zombieScore : null);
             }
         }
     }
