7fb43e5c2b4ea9ca1fe8743a44d39dd4198f8dda
AlexSaskevich <saskevich.aleksey@gmail.com>
2023-05-12 19:56:28 +0300

    Add textures for menu

--- Assets/Source/UI/Buttons/UIButtons/BackButton.cs
+++ Assets/Source/UI/Buttons/UIButtons/BackButton.cs
@@ -6,10 +6,12 @@ namespace Source.UI.Buttons.UIButtons
     public sealed class BackButton : UIButton
     {
         [SerializeField] private UIView _uiViewToHide;
+        [SerializeField] private UIView _uiViewToShow;
 
         protected override void OnButtonClick()
         {
             _uiViewToHide.Hide();
+            _uiViewToShow.Show();
         }
     }
 }
--- Assets/Source/UI/Buttons/UIButtons/PlayButton.cs
+++ Assets/Source/UI/Buttons/UIButtons/PlayButton.cs
@@ -6,9 +6,11 @@ namespace Source.UI.Buttons.UIButtons
     public sealed class PlayButton : UIButton
     {
         [SerializeField] private GameModeView _gameModeView;
+        [SerializeField] private MenuView _menuView;
 
         protected override void OnButtonClick()
         {
+            _menuView.Hide();
             _gameModeView.Show();
         }
     }
