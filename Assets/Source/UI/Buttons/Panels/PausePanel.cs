9dcbcd8c05a072e9ec99ee6f45a212d7e342554d
daniilkalmykov <daniilk24960@gmail.com>
2023-05-12 23:25:17 +0600

    add pause panel with buttons

---
+++ Assets/Source/UI/Buttons/Panels/PausePanel.cs
@@ -0,0 +1,32 @@
+﻿using Source.UI.Buttons.UIButtons;
+using UnityEngine;
+
+namespace Source.UI.Buttons.Panels
+{
+    public sealed class PausePanel : MonoBehaviour
+    {
+        [SerializeField] private ContinueButton _continueButton;
+        [SerializeField] private ExitButton _exitButton;
+
+        private void Awake()
+        {
+            _continueButton.transform.SetParent(transform);
+            _exitButton.transform.SetParent(transform);
+        }
+
+        private void OnEnable()
+        {
+            _continueButton.Continued += OnContinued;
+        }
+
+        private void OnDisable()
+        {
+            _continueButton.Continued += OnContinued;
+        }
+
+        private void OnContinued()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
