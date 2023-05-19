d81bfb0a8494440cdfe22487e9447367d32e6ab5
daniilkalmykov <daniilk24960@gmail.com>
2023-05-19 18:19:59 +0600

    fix time mode score

--- Assets/Source/GameLogic/Timers/FirstGameModeBlinder.cs
+++ Assets/Source/GameLogic/Timers/FirstGameModeBlinder.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Source.GameLogic.Scores;
+using Source.UI.Panels;
 using UnityEngine;
 
 namespace Source.GameLogic.Timers
@@ -6,20 +8,37 @@ namespace Source.GameLogic.Timers
     public sealed class FirstGameModeBlinder : MonoBehaviour
     {
         [SerializeField] private float _time;
-
-        public event Action<FirstGameModeTimer> Initialized;
+        [SerializeField] private EndScreen _endScreen;
         
+        private TimeModeScore _timeModeScore;
+
         public FirstGameModeTimer FirstGameModeTimer { get; private set; }
+
+        private void OnDisable()
+        {
+            FirstGameModeTimer.Ended -= OnEnded;
+        }
 
         private void Start()
         {
             FirstGameModeTimer = new FirstGameModeTimer(_time);
-            Initialized?.Invoke(FirstGameModeTimer);
+            FirstGameModeTimer.Ended += OnEnded;
         }
 
         private void Update()
         {
             FirstGameModeTimer.Update(Time.deltaTime);
+        }
+
+        public void Init(TimeModeScore timeModeScore)
+        {
+            _timeModeScore = timeModeScore;
+        }
+        
+        private void OnEnded()
+        {
+            _timeModeScore.TrySetScore();
+            _endScreen.End();
         }
     }
 }
