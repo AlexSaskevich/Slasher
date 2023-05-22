68a7bb950b3231a3e81618211b6787633eb4059d
AlexSaskevich <saskevich.aleksey@gmail.com>
2023-05-22 13:22:30 +0300

    Add settings view

---
+++ Assets/Source/UI/Buttons/UIButtons/SettingsButton.cs
@@ -0,0 +1,17 @@
+﻿using Source.UI.Views;
+using UnityEngine;
+
+namespace Source.UI.Buttons.UIButtons
+{
+    public sealed class SettingsButton : UIButton
+    {
+        [SerializeField] private MenuView _menuView;
+        [SerializeField] private SettingsView _settingsView;
+
+        protected override void OnButtonClick()
+        {
+            _menuView.Hide();
+            _settingsView.Show();
+        }
+    }
+}
---
+++ Assets/Source/UI/Views/SettingsView.cs
@@ -0,0 +1,11 @@
+﻿namespace Source.UI.Views
+{
+    public sealed class SettingsView : UIView
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            Hide();
+        }
+    }
+}
