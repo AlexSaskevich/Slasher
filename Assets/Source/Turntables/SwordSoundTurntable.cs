fff409d59647ae4a1c0a8c3126f28a28af0307cd
daniilkalmykov <daniilk24960@gmail.com>
2023-05-22 18:05:57 +0600

    add sword sound

---
+++ Assets/Source/Turntables/SwordSoundTurntable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Source.Turntables
+{
+    [RequireComponent(typeof(AudioSource))]
+    public sealed class SwordSoundTurntable : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _swordSound;
+        [SerializeField] private float _minPitch;
+        [SerializeField] private float _maxPitch;
+        [SerializeField] private float _volume;
+
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlaySwordSound()
+        {
+            _audioSource.pitch = Random.Range(_minPitch, _maxPitch);
+            _audioSource.volume = _volume;
+            _audioSource.PlayOneShot(_swordSound);
+        }
+    }
+}
